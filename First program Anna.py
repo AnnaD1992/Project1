d8ee0695822b8e51e8d05d3bc60d560f7afa718f
AnnaD1992 <durbanovaanna@gmail.com>
2017-04-06 23:16:29 +0200

    Add files via upload

---
+++ First program Anna.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[5]:
+
+print("Hello ")
+print ("How was your day today?")
+
+Annas_day=input()
+print("Today your day was " + Annas_day + " but tomorrow your day will be much better")
+
+response = input()
+print("What is your name?")
+
+myName= input()
+
+print("Nice to meet you " + myName)
+print (myName + " is a very beautiful name")
+
+print("Can I ask you, how old are you?")
+age= int(input())
+
+print("Ah so... it means in 5 years you will be..." + str(int(age)+5))
+print("How do you see yourself in 5 years?")
+
+future=input()
+print("Wow, very impressive! I wish you good luck")
+
+
+# In[ ]:
+
+
+
